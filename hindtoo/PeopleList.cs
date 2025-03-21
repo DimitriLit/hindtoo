94bc050062d465d3938475b2b12decdc3e951443
opilane <opilane@HP-CZC0359NNC>
2025-03-21 10:56:08 +0200

    Add project files.

---
+++ hindtoo/PeopleList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hindtoo
+{
+    public class PeopleList
+    {
+        public static readonly List<People> people = new List<People> 
+        {
+            new People()
+            {
+                Id = 1,
+                Name = "Foo",
+                Age = 20,
+            },
+
+            new People()
+            {
+                Id = 2,
+                Name = "Bar",
+                Age = 20
+            },
+
+            new People()
+            {
+                Id = 3,
+                Name = "Jhoen",
+                Age = 26
+            },
+
+            new People()
+            {
+                Id = 4,
+                Name = "Marija",
+                Age = 26
+            },
+
+            new People()
+            {
+                Id = 5,
+                Name = "Poeter",
+                Age = 17
+            },
+
+
+
+        };
+    }
+}
---
+++ hindtoo/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using hindtoo;
+
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("");
+
+
+        var result = PeopleList.people
+            .ToLookup(x => x.Age);
+
+        foreach (var age in result)
+        {
+            Console.WriteLine("Age group: " + age.Key);
+
+            foreach (var item in age)
+            {
+                Console.WriteLine("Person name: " + item.Name);
+
+            }
+        }
+
+    }
+
+}
+
+/*
+ * Kui listis on palju inimesi kellest aeg on sama,
+ * siis ToLookUp tegib üks Key ja kui Age on sama, siis ToLookUp sorteerib sama sugune Age selles Key,
+ * aga kui Age on erineb, siis ToLookUp tegib uue Key.
+*/
